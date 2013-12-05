947c6ab4f3e16654e78a6ffa0c73d4a114d48e71
Nam Ngo <emoinrp@gmail.com>
2013-12-05 13:46:54 +1100

    Get price for different layouts

--- js/content_scripts/jbhifi.js
+++ js/content_scripts/jbhifi.js
@@ -1,6 +1,11 @@
 var product_name = document.querySelectorAll("#content-two div strong")[0].innerText;
 
-var img_src_split = document.querySelectorAll("#vmcoepetukj img")[0].src.split('/');
+try{
+    var img_src_split = document.querySelectorAll("#vmcoepetukj img")[0].src.split('/');
+} catch (e){
+    var img_src_split = document.querySelectorAll("#price_box img")[0].src.split('/');
+}
+
 var price = img_src_split[img_src_split.length - 1].split('__')[0];
 if (price.indexOf('_') != 1) {
     price = price.split('_')[0];
